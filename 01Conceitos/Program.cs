a266966cbffa75db6da3817cb9c66f102839ae2e
TulioliAles <desmecad@hotmail.com>
2020-10-05 22:05:29 -0300

    Commit 1 - Exercicio OO com C#

---
+++ 01Conceitos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01Conceitos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            p1.nome = "Alessandro";
+            p1.idade = 43;
+            p1.mensagem();
+        }
+    }
+}
